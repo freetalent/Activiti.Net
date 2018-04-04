3666b828f5a3546f489964a06e986751ab1f3715
EricXu <freetalent@foxmail.com>
2018-04-04 15:04:04 +0800

    支持Net452,NetStandard1.4

---
+++ src/Activiti.NetTest/ActivitiRESTAPITest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Rest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Activiti.Net.Test
+{
+    [TestClass]
+    public class ActivitiRESTAPITest
+    {
+        [TestMethod]
+        public void GetTablesTest()
+        {
+            BasicAuthenticationCredentials credentials = new BasicAuthenticationCredentials
+            {
+                UserName = "kermit",
+                Password = "kermit"
+            };
+
+            ActivitiRESTAPI api = new ActivitiRESTAPI(credentials);
+            var tables = api.GetTables();
+        }
+    }
+}
---
+++ src/Activiti.NetTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Activiti.NetTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Activiti.NetTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a244a4a0-4432-4c11-af3b-20b7319ffe1e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
